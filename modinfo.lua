7003bd773b91626492c73fe970f3c91d29e26811
Jaime Nufio <jaimeenufio@gmail.com>
2022-08-22 23:00:42 -0400

    init

---
+++ modinfo.lua
@@ -0,0 +1,22 @@
+-- handle chinese language
+--local L = locale ~= "zh" and locale ~= "zhr"
+--name = L and "English" or "Chinese"
+name = "Born Again"
+description = "Craftable item to be reborn"
+author = "Jaime Nufio"
+version = "1.0.0"
+
+forumthread = ""
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+hamlet_compatible = false
+all_clients_require_mod= true
+api_version = 6
+api_version_dst = 10
+server_filter_tags = {"born_again"}
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
